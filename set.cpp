4c1556ff48627f45cdd88e86afda33bbb974bee2
dorin cohen <104822601+Dorins12@users.noreply.github.com>
2022-05-28 15:09:00 +0300

    Add files via upload
    operators overloading.

---
+++ set.cpp
@@ -0,0 +1,106 @@
+﻿#include "set.h"
+#include <iostream>
+Set ::Set()
+{
+	m_max = 1000;
+	m_arry = nullptr;
+	m_cuurnetsize = 0;
+}
+Set::Set(int max)
+{
+	m_max = max;
+	m_arry = new int [m_max];
+}
+Set::Set(Set& other)
+{
+	/**this = other;*/
+	for (int i = 0; i < m_max; i++)
+	{
+		this[i] = other.m_arry[i];
+		m_cuurnetsize++;
+	}
+}
+Set::~Set()
+{
+	delete[] m_arry;
+
+}
+Set Set:: operator=(int num)
+{
+	for (int i = 0; i < m_max; i++)
+	{
+		if (m_arry[i] == num)
+		{
+			return 1;
+		}
+	}
+	return -1;
+}
+Set Set:: operator+(int num)
+{
+	int* temp;
+	temp = new int[m_cuurnetsize + 1];
+	for (int i = 0; i < m_cuurnetsize; i++)
+	{
+		temp[i] = m_arry[i];
+	}
+	temp[m_cuurnetsize] = num;
+	
+	return *temp;
+}
+Set Set:: operator+(Set&other)
+{
+	int* temp;
+	int size = m_cuurnetsize + other.m_cuurnetsize;
+	temp = new int[(size)];
+	for (int i = 0; i < size; i++)
+	{
+		if (i >= m_cuurnetsize)
+		{
+			temp[i] = other.m_arry[i];
+		}
+		temp[i] = m_arry[i];
+	}
+	return *temp;
+}
+void Set:: operator+=(int num)
+{
+	int* temp;
+	temp = new int[m_cuurnetsize + 1];
+	for (int i = 0; i < m_cuurnetsize; i++)
+	{
+		temp[i] = m_arry[i];
+	}
+	temp[m_cuurnetsize] = num;
+	m_arry = temp;
+
+}
+ostream& operator<<(ostream& os, const Set& obj)// מטודה "חברה" בגלל שאנחנו ניגשים לספרייה אחרת של הדפסה
+{
+	for (int i = 0; i < obj.m_cuurnetsize; i++)
+	{
+		os << obj.m_arry[i] << " " << endl;
+	}
+
+}
+bool Set:: operator>(Set& other)
+{
+	
+	if (m_cuurnetsize > other.m_cuurnetsize)
+	{
+		return true;
+	}
+	return false;
+}
+int Set::operator[](int index)
+{
+	/*for (int i = 0; i < m_cuurnetsize; i++)
+	{
+		if (i == index)
+		{
+			return m_arry[i];
+		}
+	}*/
+	return m_arry[index];
+}
+
---
+++ set.h
@@ -0,0 +1,27 @@
+﻿#ifndef __SET_H
+#define __SET_H
+#pragma once
+#include <iostream>
+using namespace std;
+class Set
+{
+	int *m_arry;
+	int m_max;
+	int m_cuurnetsize;
+public:
+	Set();
+	Set(int max);
+	Set(Set& other);
+	~Set();
+	Set operator=(int num);//כתיבה של אופרטור חתימה השוואה בין אובייקטים 
+	Set operator+(int num);
+	Set operator+(Set& other);//מקבל קבוצה אחרת , אובייקט אחר נשלח ככתובת וההחזר *this
+	void operator+=(int num);
+	bool operator>(Set& other);
+	friend ostream& operator<<(ostream& os, const Set& obj);// אם אני רושמת set המטודה מחזירה לי אובייקט מסוג המחלקה.
+	int operator[](int index);//חייב לרשום מה האופרטור מחזיר ואת הסימן שלו
+
+
+
+};
+#endif
